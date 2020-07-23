80a84ffb0415ec189f1edd824b9777de13dd9831
Home <Home@HOME-PC>
2020-07-23 23:35:55 +0400

    1

--- Sklad_v1_001/FormUsers/Kategor/Kategorii.xaml.cs
+++ Sklad_v1_001/FormUsers/Kategor/Kategorii.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,8 +20,42 @@ namespace Sklad_v1_001.FormUsers.Kategor
     /// <summary>
     /// Interaction logic for Kategorii.xaml
     /// </summary>
-    public partial class Kategorii : Page
+    public partial class Kategorii : Page, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public static readonly DependencyProperty VisibilityGreenProperty = DependencyProperty.Register(
+         "VisibilityZonaGreen",
+         typeof(Visibility),
+         typeof(Kategorii), new UIPropertyMetadata(Visibility.Collapsed));     
+        public Visibility IsEnableZonaGreen
+        {
+            get { return (Visibility)GetValue(VisibilityGreenProperty); }
+            set { SetValue(VisibilityGreenProperty, value); }
+        }
+        public static readonly DependencyProperty VisibilityZonaYellowProperty = DependencyProperty.Register(
+        "VisibilityZonaYellow",
+        typeof(Visibility),
+        typeof(Kategorii), new UIPropertyMetadata(Visibility.Collapsed));
+        public Visibility VisibilityZonaYellow
+        {
+            get { return (Visibility)GetValue(VisibilityZonaYellowProperty); }
+            set { SetValue(VisibilityZonaYellowProperty, value); }
+        }
+        public static readonly DependencyProperty VisibilityRedProperty = DependencyProperty.Register(
+        "VisibilityZonaRed",
+        typeof(Visibility),
+        typeof(Kategorii), new UIPropertyMetadata(Visibility.Collapsed));
+        public Visibility IsEnableZonaRed
+        {
+            get { return (Visibility)GetValue(VisibilityRedProperty); }
+            set { SetValue(VisibilityRedProperty, value); }
+        }
         public Kategorii()
         {
             InitializeComponent();
