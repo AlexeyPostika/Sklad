590a028cf8bb2ba9386a6e2f93348ad61b9143ad
Алексей Постика <alex_al_oper@mail.ru>
2021-07-08 11:59:06 +0300

    3

--- Sklad_v1_001/FormUsers/SupplyDocument/SupplyDocumentLogic.cs
+++ Sklad_v1_001/FormUsers/SupplyDocument/SupplyDocumentLogic.cs
@@ -1,4 +1,5 @@
 ﻿using Sklad_v1_001.GlobalVariable;
+using Sklad_v1_001.HelperGlobal;
 using Sklad_v1_001.SQL;
 using System;
 using System.Collections.Generic;
@@ -621,8 +622,91 @@ namespace Sklad_v1_001.FormUsers.SupplyDocument
         }
     }
 
+    public class RowsFilters: INotifyPropertyChanged
+    {
+        private Double filtersQuantityMin;
+        private Double filtersQuantityMax;
+        private Double filtersAmountMin;
+        private Double filtersAmountMax;
+
+        public Double FiltersQuantityMin
+        {
+            get
+            {
+                return filtersQuantityMin;
+            }
+
+            set
+            {
+                filtersQuantityMin = value;
+                OnPropertyChanged("FiltersQuantityMin");
+            }
+        }
+        public Double FiltersQuantityMax
+        {
+            get
+            {
+                return filtersQuantityMax;
+            }
+
+            set
+            {
+                filtersQuantityMax = value;
+                OnPropertyChanged("FiltersQuantityMax");
+            }
+        }
+        public Double FiltersAmountMin
+        {
+            get
+            {
+                return filtersAmountMin;
+            }
+
+            set
+            {
+                filtersAmountMin = value;
+                OnPropertyChanged("FiltersAmountMin");
+            }
+        }
+        public Double FiltersAmountMax
+        {
+            get
+            {
+                return filtersAmountMax;
+            }
+
+            set
+            {
+                filtersAmountMax = value;
+                OnPropertyChanged("FiltersAmountMax");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
     public class SupplyDocumentLogic
     {
+        ConvertData convertData;
+
+        public DataTable innerList1;
+        public DataTable innerList2;
+        public DataTable innerList3;
+        public DataTable innerList4;
+        public DataTable innerList5;
+
+        public class Range
+        {
+            public double min;
+            public double max;
+        }
+
+        Dictionary<String, DataTable> filters;
+        Dictionary<String, Range> filtersFromTo;
+
         string get_store_procedure = "xp_GetSupplyDocumentTable";
         string get_filters_procedure = "xp_GetSupplyDocumentFilter";
         string get_summary_procedure = "xp_GetSupplyDocumentSummary";
@@ -637,6 +721,46 @@ namespace Sklad_v1_001.FormUsers.SupplyDocument
 
         public SupplyDocumentLogic()
         {
+            convertData = new ConvertData();
+
+            filters = new Dictionary<string, DataTable>();
+            filtersFromTo = new Dictionary<string, Range>();
+
+            innerList1 = new DataTable();
+            innerList2 = new DataTable();
+            innerList3 = new DataTable();
+            innerList4 = new DataTable();
+            innerList5 = new DataTable();
+
+            innerList1.Columns.Add("ID");
+            innerList1.Columns.Add("IsChecked");
+            innerList1.Columns.Add("Description");
+
+            innerList2.Columns.Add("ID");
+            innerList2.Columns.Add("IsChecked");
+            innerList2.Columns.Add("Description");
+
+            innerList3.Columns.Add("ID");
+            innerList3.Columns.Add("IsChecked");
+            innerList3.Columns.Add("Description");
+
+            innerList4.Columns.Add("ID");
+            innerList4.Columns.Add("IsChecked");
+            innerList4.Columns.Add("Description");
+
+            innerList5.Columns.Add("ID");
+            innerList5.Columns.Add("IsChecked");
+            innerList5.Columns.Add("Description");
+
+            filters.Add("CreatedByUserID", innerList1);
+            filters.Add("LastModifiedByUserID", innerList1);
+            filters.Add("Delivery", innerList1);
+            filters.Add("ManagerName", innerList1);
+            filters.Add("Status", innerList1);
+
+            Range QuantityRange = new Range();
+            Range AmountRange = new Range();         
+
             _sqlRequestSelect = new SQLCommanSelect();
             _sqlRequestSelectFilters = new SQLCommanSelect();
             _sqlRequestSelectSummary = new SQLCommanSelect();
@@ -818,6 +942,60 @@ namespace Sklad_v1_001.FormUsers.SupplyDocument
             _sqlRequestSelectSummary.ComplexRequest(get_summary_procedure, CommandType.StoredProcedure, null);
             _data = _sqlRequestSelectSummary.SqlAnswer.datatable;
             return _data;
+        }
+
+        public RowsFilters ConvertFilter(DataRow _dataRow, RowsFilters _localeRow)
+        {
+            ConvertData convertData = new ConvertData(_dataRow, _localeRow);
+            _localeRow.FiltersQuantityMin = convertData.ConvertDataDouble("QuantityMin");
+            _localeRow.FiltersQuantityMax = convertData.ConvertDataDouble("QuantityMax");
+            _localeRow.FiltersAmountMin = convertData.ConvertDataDouble("AmountMin");
+            _localeRow.FiltersAmountMax = convertData.ConvertDataDouble("AmountMax");
+            return _localeRow;
+        }
+
+        public void InitFilters()
+        {
+            DataTable table = FillGridAllFilter();
+            FillFilter("CreatedByUserID", table);
+            FillFilter("LastModifiedByUserID", table);
+            FillFilter("Delivery", table);
+            FillFilter("ManagerName", table);
+            FillFilter("Status", table);
+
+            RowsFilters rowsFilters;
+            foreach(DataRow row in table.Rows)
+            {
+                rowsFilters = new RowsFilters();
+                ConvertFilter(row, rowsFilters);
+                filtersFromTo["Quantity"].min = rowsFilters.FiltersQuantityMin;
+                filtersFromTo["Quantity"].max = rowsFilters.FiltersQuantityMax;
+                filtersFromTo["Amount"].min = rowsFilters.FiltersAmountMin;
+            }
+        }
+
+        public void FillFilter(String filterName, DataTable table) 
+        {
+            DataTable current1 = filters.FirstOrDefault(x => x.Key == filterName).Value;
+            current1.Clear();
+            foreach(DataRow row in table.Rows)
+            {
+                ConvertData convert = new ConvertData(row, new LocalRow());
+                if (!String.IsNullOrEmpty(row[filterName].ToString()))
+                {
+                    String data = convert.ConvertDataString(filterName);
+                    String[] dataarray = data.Split('|');
+                    foreach(string curstr in dataarray.ToList())
+                    {
+                        String[] pair = curstr.Split(':');
+                        DataRow newrow = current1.NewRow();
+                        newrow["ID"] = pair[0];
+                        newrow["IsChecked"] = false;
+                        newrow["Description"] = pair[1];
+                        current1.Rows.Add(newrow);
+                    }
+                }
+            }
         }
     }
 }
