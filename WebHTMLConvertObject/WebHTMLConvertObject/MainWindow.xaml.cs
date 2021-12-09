a5b1f07a291e54dd0b1aa4461f1025cc871204f2
Алексей Постика <alex_al_oper@mail.ru>
2021-12-09 18:58:09 +0300

    3

--- WebHTMLConvertObject/WebHTMLConvertObject/MainWindow.xaml.cs
+++ WebHTMLConvertObject/WebHTMLConvertObject/MainWindow.xaml.cs
@@ -49,6 +49,7 @@ namespace WebHTMLConvertObject
         private void Request_Click(object sender, RoutedEventArgs e)
         {
             Content.Text = String.Empty;
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; " + "Windows NT 5.2; .NET CLR 1.0.3705;)");
             client.DownloadDataAsync(new Uri(URLText.Text));
 
             //client.OpenReadAsync(new Uri(URLText.Text));
