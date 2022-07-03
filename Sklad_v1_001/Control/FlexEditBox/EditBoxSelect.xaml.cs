0999bd0ed6d6b60182e45d0f5660e2b186268db0
Алексей Постика <alex_al_oper@mail.ru>
2022-07-03 20:40:01 +0300

    3

---
+++ Sklad_v1_001/Control/FlexEditBox/EditBoxSelect.xaml.cs
@@ -0,0 +1,98 @@
+﻿using POS.GlobalVariable;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace POS.FlexControls.FlexEditBox
+{
+    /// <summary>
+    /// Логика взаимодействия для EditBoxDelete.xaml
+    /// </summary>
+    public partial class EditBoxSelect : UserControl
+    {
+        // свойство зависимостей
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+                        "Text",
+                        typeof(string),
+                        typeof(EditBoxSelect), new UIPropertyMetadata(String.Empty));
+
+        // Обычное свойство .NET  - обертка над свойством зависимостей
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public EditBoxSelect()
+        {
+            InitializeComponent();
+            image.Source = ImageHelper.GenerateImage("IconSelect.png");
+        }
+
+        public event Action ButtonSelectClick;
+        public event Action ButtonLostFocusClick;
+
+        public Boolean IsReadOnly
+        {
+            get
+            {
+                return TextField.IsReadOnly;
+            }
+
+            set
+            {
+                TextField.IsReadOnly = value;
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!TextField.IsReadOnly)
+                ButtonSelectClick?.Invoke();
+        }
+
+        private void TextField_LostFocus(object sender, RoutedEventArgs e)
+        {
+            ButtonLostFocusClick?.Invoke();
+        }
+
+        private void TextField_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F5)
+            {
+                ButtonSelectClick?.Invoke();
+            }
+        }
+
+        private void TextField_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Text == Properties.Resources.BadData)
+            {
+                TextField.BorderBrush = Brushes.Red;
+                TextField.Opacity = 0.5;
+            }
+            else
+            {
+                TextField.Opacity = 1;
+                TextField.Style = (Style)MainWindow.AppWindow.TryFindResource("TextBoxStyle");
+                SetterBaseCollection test = TextField.Style.Setters;
+                foreach (Setter test1 in test)
+                {
+                    if (test1.Property.ToString() == "BorderBrush")
+                        TextField.BorderBrush = (Brush)test1.Value;
+
+                }
+            }
+        }
+
+        private void TextField_IsKeyboardFocusedChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Text == Properties.Resources.BadData)
+            {
+                Text = "";
+            }
+        }
+    }
+}
---
+++ Sklad_v1_001/Control/FlexEditBox/EditBoxSelectWithLabel.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace POS.FlexControls.FlexEditBox
+{
+    /// <summary>
+    /// Логика взаимодействия для EditBoxDelete.xaml
+    /// </summary>
+    public partial class EditBoxSelectWithLabel : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // свойство зависимостей
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+                        "Value",
+                        typeof(Int32),
+                        typeof(EditBoxSelectWithLabel), new UIPropertyMetadata(0));
+
+        // Обычное свойство .NET  - обертка над свойством зависимостей
+        public Int32 Value
+        {
+            get { return (Int32)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        // свойство зависимостей
+        public static readonly DependencyProperty DisplayValueProperty = DependencyProperty.Register(
+                        "DisplayValue",
+                        typeof(string),
+                        typeof(EditBoxSelectWithLabel), new UIPropertyMetadata(String.Empty));
+
+        // Обычное свойство .NET  - обертка над свойством зависимостей
+        public string DisplayValue
+        {
+            get { return (string)GetValue(DisplayValueProperty); }
+            set { SetValue(DisplayValueProperty, value); }
+        }
+
+        // свойство зависимостей
+        public static readonly DependencyProperty IsRequiredProperty = DependencyProperty.Register(
+                        "IsRequired",
+                        typeof(Visibility),
+                        typeof(EditBoxSelectWithLabel), new UIPropertyMetadata(Visibility.Collapsed));
+
+        // Обычное свойство .NET  - обертка над свойством зависимостей
+        public Visibility IsRequired
+        {
+            get
+            {
+                return (Visibility)GetValue(IsRequiredProperty);
+            }
+            set
+            {
+                SetValue(IsRequiredProperty, value);
+                OnPropertyChanged("IsRequired");
+            }
+        }
+
+        public EditBoxSelectWithLabel()
+        {
+            InitializeComponent();
+        }
+
+        public event Action ButtonSelectClick;
+        public event Action ButtonLostFocusSelectClick;
+
+
+        public Boolean IsReadOnly
+        {
+            get
+            {
+                return EditBoxSelect.TextField.IsReadOnly;
+            }
+
+            set
+            {
+                EditBoxSelect.TextField.IsReadOnly = value;
+            }
+        }
+
+        public string LabelText
+        {
+            get
+            {
+                return label.Content.ToString();
+            }
+
+            set
+            {
+                label.Content = value;
+            }
+        }
+
+        public double LabelWidth
+        {
+            get
+            {
+                return wrapPanel.Width;
+            }
+
+            set
+            {
+                wrapPanel.Width = value;
+            }
+        }
+
+        private void EditBoxSelect_ButtonSelectClick()
+        {
+            if (ButtonSelectClick != null)
+            {
+                ButtonSelectClick();
+            }
+        }
+
+        private void EditBoxSelect_ButtonLostFocusClick()
+        {
+            if (ButtonLostFocusSelectClick != null)
+            {
+                ButtonLostFocusSelectClick();
+            }
+        }
+    }
+}
