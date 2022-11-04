f651a03a937ab4c8a8a271976e1f43fe2ab55a28
Алексей Постика <alex_al_oper@mail.ru>
2022-11-04 11:43:56 +0300

    3

---
+++ Sklad_v1_001/FormUsers/Company/CompanyGrid.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Sklad_v1_001.GlobalAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sklad_v1_001.FormUsers.Company
+{
+    /// <summary>
+    /// Логика взаимодействия для CompanyGrid.xaml
+    /// </summary>
+    public partial class CompanyGrid : Page
+    {
+        Attributes attributes;
+        public CompanyGrid(Attributes _attributes)
+        {
+            InitializeComponent();
+
+            this.attributes = _attributes;
+        }
+    }
+}
