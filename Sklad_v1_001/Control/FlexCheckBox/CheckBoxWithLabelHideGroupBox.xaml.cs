daea27091eeefca38cc906f432a5927306430a58
Алексей Постика <alex_al_oper@mail.ru>
2022-11-16 16:52:11 +0300

    3

---
+++ Sklad_v1_001/Control/FlexCheckBox/CheckBoxWithLabelHideGroupBox.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sklad_v1_001.Control.FlexCheckBox
+{
+    /// <summary>
+    /// Логика взаимодействия для CheckBoxWithLabelHideGroupBox.xaml
+    /// </summary>
+    public partial class CheckBoxWithLabelHideGroupBox : UserControl
+    {
+        public CheckBoxWithLabelHideGroupBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
