cf2fff9a36ce28420aade4a8aec6e4ef5c27860d
Алексей Постика <alex_al_oper@mail.ru>
2022-11-24 17:39:38 +0300

    3

---
+++ Sklad_v1_001/FormUsers/Shops/ShopsGrid.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Sklad_v1_001.GlobalAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sklad_v1_001.FormUsers.Shops
+{
+    /// <summary>
+    /// Логика взаимодействия для ShopsGrid.xaml
+    /// </summary>
+    public partial class ShopsGrid : Page
+    {
+        Attributes attributes;
+        public ShopsGrid(Attributes _attributes)
+        {
+            InitializeComponent();
+            this.attributes = _attributes;
+        }
+    }
+}
