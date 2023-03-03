e330a9acd5e8a7e25fc67293d7c80da8ead1b3d6
Алексей Постика <alex_al_oper@mail.ru>
2023-03-03 18:50:19 +0300

    3

---
+++ Sklad_v1_001/Control/Stickers/FlexStickers20x10.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sklad_v1_001.Control.Stickers
+{
+    /// <summary>
+    /// Логика взаимодействия для FlexStickers20x10.xaml
+    /// </summary>
+    public partial class FlexStickers20x10 : UserControl
+    {
+        public FlexStickers20x10()
+        {
+            InitializeComponent();
+        }
+    }
+}
